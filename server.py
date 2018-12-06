2c18283f8d62d7556e90a539f544750e95ebafe3
Monthol Charattrakool <m.charattrakool@gmail.com>
2018-12-06 22:28:03 +0700

    create server.py

---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+from geoip2.database import Reader as GeoIPReader
+
+mmdb = GeoIPReader('./db/GeoLite2-City.mmdb')
+
+app = Flask(__name__)
+api = Api(app)
+
+class Index(Resource):
+    def get(self):
+        return {'message': 'OK'}
+
+class GeoIP(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('ip',type=str, location='args')
+        args = parser.parse_args()
+        res = mmdb.city(args['ip'])
+        return res.raw
+
+api.add_resource(Index, '/')
+api.add_resource(GeoIP, '/geo-ip')
+if __name__ == '__main__':
+    app.run(debug=True)
