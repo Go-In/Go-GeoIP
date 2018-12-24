d8256f5028949abf0daaeb388fe182ea5b52a4a9
Monthol Charattrakool <m.charattrakool@gmail.com>
2018-12-25 04:18:14 +0700

    enable cors

--- data-api.py
+++ data-api.py
@@ -1,16 +1,22 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 from influxdb import InfluxDBClient
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 api = Api(app)
 
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 client = InfluxDBClient('localhost', 8086, '', '', 'suricata')
 
+@cross_origin()
 class Index(Resource):
     def get(self):
         return {'message': 'OK'}
 
+@cross_origin()
 class List(Resource):
     def get(self):
         result = client.query('select * from ids;')
