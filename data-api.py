078a8344563d546795084adbffab93087564d616
monthol8th <m.charattrakool@gmail.com>
2018-12-25 04:30:21 +0700

    cors fixed

--- data-api.py
+++ data-api.py
@@ -6,29 +6,23 @@ from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 api = Api(app)
 
-CORS(api)
-CORS(app)
+CORS(app,resources={r"/*": {"origins": "*"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
-api.config['CORS_HEADERS'] = 'Content-Type'
 
 client = InfluxDBClient('localhost', 8086, '', '', 'suricata')
 
-@cross_origin()
 class Index(Resource):
     def get(self):
         return {'message': 'OK'}
 
-@cross_origin()
 class List(Resource):
     def get(self):
         result = client.query('select * from ids;')
-        print(result)
-        print(type(result))
-        return result
+        return list(result.get_points())
 
 
 
-api.add_resource(Index, '/')
+#api.add_resource(Index, '/')
 api.add_resource(List, '/list')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8014)
